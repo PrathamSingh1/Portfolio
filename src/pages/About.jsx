2461883d49ea2c7c32b43cf2cf0fe903a570e8d4
PrathamSingh1 <prathamjii36@gmail.com>
2025-09-08 08:02:04 +0000

    Added About Page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='sm:max-w-[810px] max-w-[350px] mx-auto mt-[70px]'>
+      <div>
+        <h1 className='text-[40px] leading-[52px] font-[700] mb-[30px] mt-[30px]'>About me</h1>
+
+        <p className='text-[20px] leading-[26px] text-[#ebebeb] font-[400]'>Hi, I'm Shashi Ranjan Singh a Junior Software Engineer creating modern, scalable, and user-friendly web applications. I don't have many hobbies outside coding. Whenever i got some free time i spend it on leaning new language or framework.</p>
+
+        <p className='text-[20px] mt-[20px] leading-[26px] text-[#ebebeb] font-[400]'>I focus on Full-Stack Web Development, working with technologies like Tailwind, React.js, Next.js, Node.js, Express, MongoDB, MySQL, PostgreSQL, Turborepo, AWS, Cloudflare, Vercel, Git, Github, CI/CD, Docker etc.</p>
+
+        <p className='text-[20px] mt-[20px] leading-[26px] text-[#ebebeb] font-[400]'>I am currently looking for opportunity and reaching out to the companies to join them so , if there were any openings for Junior Software Engineer in the early team i would love to be a part of the early team at your company and define its work and culture. Looking forward to hearing from you soon!</p>
+
+         <p className='text-[20px] mt-[50px] leading-[26px] text-[#3359c2] font-[400]'>Thank you for your time...</p>
+      </div>
+    </div>
   )
 }
 
